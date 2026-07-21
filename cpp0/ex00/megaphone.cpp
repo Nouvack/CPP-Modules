994cac33d4458bf0d064a0c31f6c56ae9b09182c
Nouvack <novacksantdor@gmail.com>
2026-07-21 17:24:05 +0200

    finish megaphone

--- cpp0/ex00/megaphone.cpp
+++ cpp0/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: nsantand <nsantand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/14 12:51:02 by nsantand          #+#    #+#             */
-/*   Updated: 2026/07/17 12:26:18 by nsantand         ###   ########.fr       */
+/*   Updated: 2026/07/21 15:23:27 by nsantand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,14 @@
 
 int main(int argc, char  **argv)
 {
-    char *line = NULL;
     
     if(argc == 1)
-    {
         return(std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl, 0);
-    }
     for(int i = 1; i < argc; i++)
     {
         for(int j = 0; argv[i][j]; j++)
-        {
             std::cout << static_cast<char>(std::toupper(static_cast<unsigned char>(argv[i][j])));
-        }
     }
-    std::cout << line << std::endl;
+    std::cout << std::endl;
     return(0);
 }
