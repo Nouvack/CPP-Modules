6c907f123219e9ce470ea9b39f756e58c51ccadf
Nouvack <novacksantdor@gmail.com>
2026-08-24 12:01:42 +0200

    fear: finish cpp01

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -11,21 +11,25 @@
 /* ************************************************************************** */
 
 #include <iostream>
+#include <iostream>
+
 int main(void)
 {
     std::string str = "HI THIS IS BRAIN";
     std::string* stringPTR = &str;
     std::string& stringREF = str;
 
-    std::cout << "Original Variable" << std::endl;
-    std::cout << str << std::endl;
-    std::cout << "------------------------------------------" << std::endl;
-    std::cout << "A pointer to the Variable" << std::endl;
-    std::cout << stringPTR << std::endl;
-    std::cout << "------------------------------------------" << std::endl;
-    std::cout << "A reference to the Variable" << std::endl;
-    std::cout << stringREF << std::endl;
+    // --- BLOQUE 1: DIRECCIONES ---
+    std::cout << "The memory address of the string variable: " << &str << std::endl;
+    std::cout << "The memory address held by stringPTR:      " << stringPTR << std::endl;
+    std::cout << "The memory address held by stringREF:      " << &stringREF << std::endl;
     
+    std::cout << "---------------------------------------------------" << std::endl;
+    
+    // --- BLOQUE 2: VALORES ---
+    std::cout << "The value of the string variable:  " << str << std::endl;
+    std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
 
     return 0;
 }
